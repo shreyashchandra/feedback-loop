bb0e22c31cf7ae801e0212bd6c763e718942c4f8
shreyash <shreyash_c@yahoo.com>
2024-09-15 18:53:14 +0530

    DB connection created

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 #TODO:- Feedback-Loop
 - [x] Create DB Model (UserModel and MessageModel)
 - [x] Create Validation Schema (signUp, signIn, verify, acceptMessage, message)
+- [x] DB connection created
+- [ ]
+- [ ]
 - [ ]
 - [ ]
---
+++ src/lib/dbConnects.ts
@@ -0,0 +1,30 @@
+ import mongoose from "mongoose";
+
+ type ConnectionObject = {
+    isConnected?: number;
+ }
+
+ const connection: ConnectionObject = {}
+
+ async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("Already Db connected");
+        return;     
+    }
+    try {
+       const db = await mongoose.connect(process.env.MONGODB_URI??"");
+
+       console.log(db);
+       connection.isConnected = db.connections[0].readyState;
+
+       console.log("DB Connected Successfully");
+       
+       
+        
+    } catch (error) {
+        console.log("DB Connected failed");
+        process.exit(1);
+    }
+ }
+
+ export default dbConnect;
